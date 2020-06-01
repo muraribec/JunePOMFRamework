b1607d7cb89e18fc479aaf54e6eb28439a520ccd
murari <muraribec@gmail.com>
2020-06-01 07:03:42 +0530

    updated the code as per the pr review comments

--- src/com/java/PrimeNUmberGIT.java
+++ src/com/java/PrimeNUmberGIT.java
@@ -5,6 +5,7 @@ public class PrimeNUmberGIT {
 	public static void main(String[] args) {
 
 		System.out.println("Prime Number");
+		System.out.println("new modified code as per PR comments");
 	}
 
 }
